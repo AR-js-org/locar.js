ef075f735fba62170731de8bde9cb28a814136d8
Nick Whitelegg <nickw4426@gmail.com>
2026-04-18 20:34:49 +0100

    Remove testing code from example 1

--- examples/01-helloworld/src/main.ts
+++ examples/01-helloworld/src/main.ts
@@ -6,13 +6,8 @@ import { LocAR, App, ReadyEvent} from 'locar';
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.001, 100);
 const app = new App({ camera });
 
-const locar1 = new LocAR(new THREE.Scene(), camera);
-locar1.fakeGps(1,1);
-
 app.on("ready", (ev: ReadyEvent) => {
     const { locar } = ev;
-    console.log("ready callback");
-    console.log(locar);
     locar.fakeGps(-0.72, 51.05);
     const geom = new THREE.BoxGeometry(10, 10, 10);
     const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
